6097185ff93fa0c116c57331c2b45e9067d2de75
mariosknl <mariosknl@outlook.com>
2020-08-03 16:12:19 +0300

    Changed App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">This is my calculator in ReactJS.</div>;
 }
 
 export default App;
