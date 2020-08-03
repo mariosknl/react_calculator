36e8047fdf27651327c1630292216db5f730b2c8
mariosknl <mariosknl@outlook.com>
2020-08-03 16:49:45 +0300

    Stickler config

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App">This is my calculator in ReactJS.</div>;
+  return <div className="App">This is my  in ReactJS.</div>;
 }
 
 export default App;
