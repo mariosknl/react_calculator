00586c2dd1eccb58c71316dd7dec4761de811583
mariosknl <mariosknl@outlook.com>
2020-08-03 16:44:37 +0300

    Set up eslint & stylelint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 
-import "./App.css";
+import './App.css';
 
 function App() {
   return <div className="App">This is my calculator in ReactJS.</div>;
